5a67d828de210139a03d379ce40085d08eb7a1cf
Shyamali Samant <shyamalisamant88@gmail.com>
2026-07-01 12:15:39 +0530

    Time: 504 ms (7.49%), Space: 53 MB (44.53%) - LeetHub

---
+++ 0436-find-right-interval/0436-find-right-interval.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] findRightInterval(int[][] intervals) {
+        int n=intervals.length;
+        int m=intervals[0].length;
+        int ans[] = new int[n];
+        for(int i=0;i<n;i++){
+                int min=Integer.MAX_VALUE;
+                ans[i]=-1;
+            for(int j=0;j<n;j++){     
+                if(intervals[j][0]>=intervals[i][1]){
+                    if(intervals[j][0] < min){
+                        min=intervals[j][0];
+                       ans[i]=j;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
