4413d1a35491b61efcfe01132dbfeebf8b72dadb
Shyamali Samant <shyamalisamant88@gmail.com>
2026-07-08 23:42:29 +0530

    Time: 1 ms (99.72%), Space: 47.5 MB (21.83%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int jump(int[] nums) {
+        int n =nums.length;
+        if(n==1) return 0;
+        int max=0;
+        int curr=0;
+        int count=0;
+        for(int i=0; i<n-1; i++){
+             max=Math.max(max,i+nums[i]);
+             if(i==curr){
+                count++;
+                curr=max;
+             }
+        }
+        return count;
+    }
+}
