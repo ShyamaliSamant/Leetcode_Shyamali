d9a03b24cc894f063152147947de7eb13dbfbeea
Shyamali Samant <shyamalisamant88@gmail.com>
2026-07-26 23:31:49 +0530

    Time: 8 ms (15.17%), Space: 48 MB (60.72%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num: nums){
+            map.put(num , map.getOrDefault(num,0)+1);
+        }
+        int ans[] = new int[2];
+        int i=0;
+
+        for(int key : map.keySet()){
+            if(map.get(key) ==1)
+            ans[i++]= key;
+        }
+        return ans;
+    }
+}
