78cf79b0346991ea0de91cf9fc34d13cd9146e79
Shyamali Samant <shyamalisamant88@gmail.com>
2026-08-04 21:11:39 +0530

    Time: 3 ms (70.58%), Space: 47.1 MB (23.77%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -1,17 +1,29 @@
 class Solution {
+    int[] dp;
+    public int f(int[] temp, int idx) {
+        if (idx >= temp.length) return 0;
+
+        if (dp[idx] != -1) return dp[idx];
+
+        int pick = temp[idx] + f(temp, idx + 2);
+        int skip = f(temp, idx + 1);
+
+        return dp[idx] = Math.max(pick, skip);
+    }
+
     public int deleteAndEarn(int[] nums) {
-         int n = 10001;
-        int[] values = new int[n];
-        for (int num : nums){
-            values[num] += num;
+        int max = 0;
+        for (int num : nums) {
+            max = Math.max(max, num);
         }
-        int take = 0, skip = 0;
-        for (int i = 0; i < n; i++) {
-            int takei = skip + values[i];
-            int skipi = Math.max(skip, take);
-            take = takei;
-            skip = skipi;
+
+        int[] temp = new int[max + 1];
+        for (int num : nums) {
+            temp[num] += num;
         }
-        return Math.max(take, skip);
+
+        dp = new int[max + 1];
+        Arrays.fill(dp, -1);
+        return f(temp, 1);
     }
 }
