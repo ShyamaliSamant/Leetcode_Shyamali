425b42c6f901ebae06f1ce69b2be562c4880331e
Shyamali Samant <shyamalisamant88@gmail.com>
2026-08-07 22:09:55 +0530

    Time: 11 ms (96.18%), Space: 46.1 MB (93.2%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,24 +1,21 @@
 class Solution {
-    int[][] dp;
-    int INF = (int)1e9;
-    int solve(int[] coins, int idx, int amount) {
-        if (amount == 0)
-            return 0;
-        if (idx < 0)
-            return INF;
-        if (dp[idx][amount] != -1)
-            return dp[idx][amount];
-        int notTake = solve(coins, idx - 1, amount);
-        int take = INF;
-        if (coins[idx] <= amount)
-            take = 1 + solve(coins, idx, amount - coins[idx]);
-        return dp[idx][amount] = Math.min(take, notTake);
-    }
     public int coinChange(int[] coins, int amount) {
-        dp = new int[coins.length][amount + 1];
-        for (int[] row : dp)
-            java.util.Arrays.fill(row, -1);
-        int ans = solve(coins, coins.length - 1, amount);
-        return ans >= INF ? -1 : ans;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int coin : coins) {
+            for (int currAmount = coin; currAmount <= amount; currAmount++) {
+
+                if (dp[currAmount - coin] != Integer.MAX_VALUE) {
+
+                    dp[currAmount] = Math.min(
+                        dp[currAmount],
+                        1 + dp[currAmount - coin]
+                    );
+                }
+            }
+        }
+
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
     }
 }
