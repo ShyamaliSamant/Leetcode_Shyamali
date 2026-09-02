bf1e57ffaaeea59619a86f0f92feb32e910f81e1
Shyamali Samant <shyamalisamant88@gmail.com>
2026-09-02 15:01:15 +0530

    Time: 0 ms (100%), Space: 46.6 MB (79.22%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,29 +1,20 @@
 class Solution {
+    int[][] dir = {{1,0},{0,1},{-1,0},{0,-1}};
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-     int n=image.length;
-     int m= image[0].length;
-     int oldColor = image[sr][sc];
-     if(oldColor == color) return image;
-     Queue<int[]> q = new LinkedList<>();
-     q.offer(new int[]{sr,sc});
-     int[][] dir={{1,0},{0,1},{-1,0},{0,-1}};
-     while(!q.isEmpty()){
-        int[] curr =q.poll();
-        int r =curr[0];
-        int c = curr[1];
-        image[r][c]=color;
-         for (int[] d : dir) {
-                int nr = r + d[0];
-                int nc = c + d[1];
-                if (nr >= 0 && nr < n &&
-                    nc >= 0 && nc < m &&
-                    image[nr][nc] == oldColor) {
-                    q.offer(new int[]{nr, nc});
-                    // Mark as visited immediately
-                    image[nr][nc] = color;
-     }
-         }
-     }
-     return image;
+      int oldcolor = image[sr][sc];
+      if(oldcolor == color) return image;
+
+        dfs(image,sr,sc,oldcolor,color);
+      return image;
+    }
+      private void dfs(int[][] image, int r, int c, int oldcolor, int color){
+        image[r][c] = color;
+        for(int[] d: dir){
+            int nr = r + d[0];
+            int nc = c + d[1];
+             if (nr >= 0 && nr < image.length &&nc >= 0 && nc < image[0].length && image[nr][nc] == oldcolor) {
+                dfs(image, nr, nc, oldcolor, color);
+        }
+      }
     }
 }
